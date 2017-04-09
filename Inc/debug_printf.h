d86258e14ad22c1bb0e753ce84a88c0fb111ff55
dizcza <dizcza@gmail.com>
2017-04-09 19:37:40 +0300

    added printf float for mat4 and vec4

--- Inc/debug_printf.h
+++ Inc/debug_printf.h
@@ -8,37 +8,44 @@
 #ifndef DEBUG_PRINTF_H_
 #define DEBUG_PRINTF_H_
 
-#define DEBUG1
+#define DEBUG
  #ifdef DEBUG
-     #include "linmath.h"
+	 #include "linmath.h"
      #include <stdio.h>
      #include <stdlib.h>
      #define db_puts(s) puts(s)
      #define db_printf(szFormat, ...) printf(szFormat,##__VA_ARGS__)
+	 extern void initialise_monitor_handles(void);
 
-	#define DB_DEFINE_PRINTF_VECI(n,pref,b) \
-	static void inline db_printf_vec##n##pref##int##b(vec##n##pref##int##b const v) { \
-		uint8_t i; \
-		db_printf("vec "); \
+#define DB_DEFINE_PRINTF(n) \
+static void inline db_printf_vec##n(vec##n const v) { \
+	uint8_t i; \
+	db_printf("vec%d { ", n); \
+	for (i = 0; i < n; ++i) { \
+		db_printf("%f ", v[i]); \
+	} \
+	db_printf(" }\n"); \
+} \
+static inline void db_printf_mat##n##x##n(mat##n##x##n const M) { \
+	uint8_t i, j; \
+	db_printf("mat%dx%d", n, n); \
+	for (j = 0; j < n; ++j) { \
+		db_printf("\t"); \
 		for (i = 0; i < n; ++i) { \
-			db_printf("%d ", v[i]); \
+			db_printf("%f ", M[i][j]); \
 		} \
 		db_printf("\n"); \
-	}
+	} \
+	db_printf("\n"); \
+}
 
-	 extern void initialise_monitor_handles(void);
  #else
-     #define DB_DEFINE_PRINTF_VECI(n,pref,b)
      #define db_puts(s)
      #define db_printf(szFormat, ...)
+ #define DB_DEFINE_PRINTF(n)
  #endif
 
-	 DB_DEFINE_PRINTF_VECI(2,,16);
-	 DB_DEFINE_PRINTF_VECI(3,,16);
-	 DB_DEFINE_PRINTF_VECI(4,,16);
-
-	 DB_DEFINE_PRINTF_VECI(2,u,32);
-	 DB_DEFINE_PRINTF_VECI(3,u,32);
-	 DB_DEFINE_PRINTF_VECI(4,u,32);
+DB_DEFINE_PRINTF(3);
+DB_DEFINE_PRINTF(4);
 
 #endif /* DEBUG_PRINTF_H_ */
